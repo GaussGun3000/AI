aca61fac77ef8affb7d60a3f7da78c0c0b3637c7
immariav <mariavolynkina03@gmail.com>
2023-11-15 00:10:23 +0300

    mainwindow wip

--- AI/MainWindow.cpp
+++ AI/MainWindow.cpp
@@ -1,19 +1,20 @@
 #include "MainWindow.h"
-#include "Node.h"
 #include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    setStyleSheet("* { font-size: 16px; }");
+    setStyleSheet("* { font-size: 15px; }");
     mutex.lock();
     QVector<int> startState = { 6, 0, 8, 5, 2, 1, 4, 3, 7 };
     QVector<int> targetState = { 1, 2, 3, 8, 0, 4, 7, 6, 5 };
     //QVector<int> targetState = { 6, 2, 8, 5, 3, 1, 4, 0, 7 };
     int maxDepth = 500000;
-
-    //connect(bds, &BiDirectionalSearch::updateStats, this, &MainWindow::updateStatLabels);
-    //connect(bds, &BiDirectionalSearch::finished, this, &MainWindow::updateFinishedStatLabelsBDS);
+    gsh1 = new GreedySearch(&mutex, GreedySearch::HFunction::h1, startState, targetState);
+    gsh2 = new GreedySearch(&mutex, GreedySearch::HFunction::h2, startState, targetState);
+   // astar = new
+    connect(gsh1, &GreedySearch::updateStats, this, &MainWindow::updateStatLabels);
+    connect(gsh1, &GreedySearch::finished, this, &MainWindow::updateFinishedStatLabels);
 
     ui.setupUi(this);
     ui.stepButton->setDisabled(true);
@@ -49,11 +50,11 @@ void MainWindow::startButtonClicked()
     auto mode = ui.typeComboBox->currentIndex();
     if (static_cast<SearchMode>(mode) == SearchMode::Greedyh1)
     {
-        
+        gsh1->start();
     }
-    else
+    else if (static_cast<SearchMode>(mode) == SearchMode::Greedyh2)
     {
-        
+        gsh2->start();
     }
    
     update();
@@ -63,26 +64,24 @@ void MainWindow::updateStatLabels(quint32 depth)
 {
     ui.depthLabel->setText(QString::number(depth));
     auto mode = ui.typeComboBox->currentIndex();
-    if (static_cast<SearchMode>(mode) == SearchMode::Greedyh1)
+    if (static_cast<SearchMode>(mode) == SearchMode::Greedyh1 || static_cast<SearchMode>(mode) == SearchMode::Greedyh2)
     {
-        ui.timeComplexityLabel->setText(QString::number(bds->getStepCount()));
-        ui.capacitiveComplexityLabel->setText(QString::number(bds->getNodeCount()));
+        ui.timeComplexityLabel->setText(QString::number(gsh1->getStepCount()));
+        ui.capacitiveComplexityLabel->setText(QString::number(gsh1->getNodeCount()));
         if (this->inSingleStepMode)
         {
-            ui.startDirStateLabel->setText(QString(bds->getLastStartNodeStateString()));
-            ui.targetDirStateLabel->setText(QString(bds->getLastTargetNodeStateString()));
-            if (bds->getStepCount() > 1)
+            ui.lastStateLabel->setText(QString(gsh1->getLastNodeStateString()));
+            if (gsh1->getStepCount() > 1)
             {
-                ui.parentStartDirStateLabel->setText(QString(bds->getParentLastStartNodeStateString()));
-                ui.parentTargetDirStateLabel->setText(QString(bds->getParentLastTargetNodeStateString()));
+                ui.parentStateLabel->setText(QString(gsh1->getParentLastNodeStateString()));
             } 
             else
             {
-                ui.parentStartDirStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
-                ui.parentTargetDirStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
+                ui.parentStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
             }
         }
     }
+    /*
     else
     {
         ui.timeComplexityLabel->setText(QString::number(dfs->getStepCount()));
@@ -97,49 +96,26 @@ void MainWindow::updateStatLabels(quint32 depth)
         }
             
     }
+    */
 }
 
-void MainWindow::updateFinishedStatLabelsBDS()
+void MainWindow::updateFinishedStatLabels()
 {
-    uint32_t resDepth = bds->getResultingDepth();
+    uint32_t resDepth = gsh1->getResultingDepth();
     if (resDepth == -1)
         ui.searchStatusLabel->setText(QString::fromLocal8Bit("Поиск завершен. Решение не найдено"));
     else
         ui.searchStatusLabel->setText(QString::fromLocal8Bit("Поиск завершен. Решение найдено"));
-    ui.depthLabel->setText(QString::number(bds->getResultingDepth()));
-    ui.startDirStateLabel->setText(QString(bds->getLastStartNodeStateString()));
-    ui.targetDirStateLabel->setText(QString(bds->getLastTargetNodeStateString()));
-    if (bds->getStepCount() > 1)
+    ui.depthLabel->setText(QString::number(gsh1->getResultingDepth()));
+    ui.lastStateLabel->setText(QString(gsh1->getLastNodeStateString()));
+    if (gsh1->getStepCount() > 1)
     {
-        ui.parentStartDirStateLabel->setText(QString(bds->getParentLastStartNodeStateString()));
-        ui.parentTargetDirStateLabel->setText(QString(bds->getParentLastTargetNodeStateString()));
+        ui.parentStateLabel->setText(QString(gsh1->getParentLastNodeStateString()));
     }
     else
     {
-        ui.parentStartDirStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
-        ui.parentTargetDirStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
+        ui.parentStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
     }
-    update();
-    ui.stepButton->setDisabled(true);
-    ui.completeButton->setDisabled(true);
-    ui.startButton->setDisabled(false);
-    ui.typeComboBox->setDisabled(false);
-    mutex.lock();
-}
-
-void MainWindow::updateFinishedStatLabelsDFS()
-{
-    uint32_t resDepth = dfs->getResultingDepth();
-    if (resDepth == -1)
-        ui.searchStatusLabel->setText(QString::fromLocal8Bit("Поиск завершен. Решение не найдено"));
-    else
-        ui.searchStatusLabel->setText(QString::fromLocal8Bit("Поиск завершен. Решение найдено"));
-    ui.depthLabel->setText(QString::number(dfs->getResultingDepth()));
-    ui.startDirStateLabel->setText(QString(dfs->getLastNodeStateString()));
-    if (dfs->getStepCount() > 1)
-        ui.parentStartDirStateLabel->setText(QString(dfs->getParentLastNodeStateString()));
-    else
-        ui.parentStartDirStateLabel->setText(QString::fromLocal8Bit("Это начальный узел"));
     update();
     ui.stepButton->setDisabled(true);
     ui.completeButton->setDisabled(true);
--- AI/MainWindow.h
+++ AI/MainWindow.h
@@ -2,7 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_MainWindow.h"
-#include "BidirectionalDFS.h"
+#include "GreedySearch.h"
 
 class MainWindow : public QMainWindow
 {
@@ -14,19 +14,19 @@ public:
 
 private:
     Ui::MainWindowClass ui;
-    BiDirectionalSearch* bds;
-    DFS* dfs;
+    GreedySearch* gsh1;
+    GreedySearch* gsh2;
+    //AStar* astar;
     QMutex mutex;
     bool inSingleStepMode;
-    enum class SearchMode {Greedyh1=0, Greedyh2=1, Ah1=2, Ah2=3};
+    enum class SearchMode { Greedyh1=0, Greedyh2=1, Ah1=2, Ah2=3 };
 
 public slots:
     void stepButtonClicked();
     void completeButtonClicked();
     void startButtonClicked();
     void updateStatLabels(quint32 depth);
-    void updateFinishedStatLabelsBDS();
-    void updateFinishedStatLabelsDFS();
+    void updateFinishedStatLabels();
     void modeChanged();
 
 signals:
